50528c5f95952e9632e02dc7a4044bed59933697
mehmetoguz <sbrtsoguz@gmail.com>
2023-04-02 18:08:01 +0300

    020423

---
+++ src/FOR_WHİLE_DoWHİLE/SORU_2_WHİLE.java
@@ -0,0 +1,21 @@
+package FOR_WHİLE_DoWHİLE;
+
+import java.util.Scanner;
+
+public class SORU_2_WHİLE {
+    public static void main(String[] args) {
+        // Kullanicidan toplanmak uzere sayilar isteyin toplam 500 olur veya gecerse
+        //toplami yazdirin.
+        Scanner scan=new Scanner(System.in);
+        int sayi=0;
+        int toplam=0;
+        while (toplam<=500){
+            System.out.println("Toplanmak üzere sayılar giriniz");
+            sayi= scan.nextInt();
+            toplam+=sayi;
+        }
+        System.out.println("Sayılar Toplamı : " + toplam);
+    }
+}
+
+
