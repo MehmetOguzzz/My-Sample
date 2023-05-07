603e38753a58bb62b754dc393c6098a877b6253a
mehmetoguz <sbrtsoguz@gmail.com>
2023-05-07 08:48:44 +0300

    060523

--- src/SWİTCH_CASE/SORU_01.java
+++ src/SWİTCH_CASE/SORU_01.java
@@ -1,34 +1,55 @@
 package SWİTCH_CASE;
 
+import java.util.Scanner;
+
 public class SORU_01 {
     public static void main(String[] args) {
 
         //Belirli bir yılın belirli bir ayındaki gün sayısını görüntülemek için bir kod yazınız.
-               // Örnek: 2000 yılının Şubat ayında gün sayısı 29.
-        int ay = 2;
-        int yil = 2000;
-        int gunSayisi = 0;
-        switch (ay) {
-            case 1: case 3: case 5:
-            case 7: case 8: case 10:
-            case 12:
-                gunSayisi = 31;
-                break;
-            case 4: case 6:
-            case 9: case 11:
-                gunSayisi = 30;
-                break;
-            case 2:
-                if (((yil % 4 == 0) && !(yil % 100 == 0)) || (yil % 400 == 0)) {
-                    gunSayisi = 29;
-                }else {
-                    gunSayisi = 28;
+        // Örnek: 2000 yılının Şubat ayında gün sayısı 29.
+
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Yıl giriniz: ");
+                int year = scanner.nextInt();
+
+                System.out.print("Ay giriniz (1-12): ");
+                int month = scanner.nextInt();
+
+                int daysInMonth = 0;
+
+                switch (month) {
+                    case 1: // Ocak ayı
+                    case 3: // Mart ayı
+                    case 5: // Mayıs ayı
+                    case 7: // Temmuz ayı
+                    case 8: // Ağustos ayı
+                    case 10: // Ekim ayı
+                    case 12: // Aralık ayı
+                        daysInMonth = 31;
+                        break;
+                    case 4: // Nisan ayı
+                    case 6: // Haziran ayı
+                    case 9: // Eylül ayı
+                    case 11: // Kasım ayı
+                        daysInMonth = 30;
+                        break;
+                    case 2: // Şubat ayı
+                        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                            // Artık yıl ise, Şubat ayının gün sayısı 29'dur
+                            daysInMonth = 29;
+                        } else {
+                            // Değilse, Şubat ayının gün sayısı 28'dir
+                            daysInMonth = 28;
+                        }
+                        break;
+                    default:
+                        System.out.println("Hatalı ay girdiniz.");
+                        return;
                 }
-                break;
-            default:
-                System.out.println("Gecersiz Ay");
-                break;
+
+                System.out.println(year + " yılının " + month + ". ayında " + daysInMonth + " gün vardır.");
+            }
         }
-        System.out.println("Gun Sayisi = " + gunSayisi);
-    }
-}
+
+
